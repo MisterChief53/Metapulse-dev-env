1144f23532a8680c5562bca39131460a6548850f
MisterChief53 <soderol3@hotmail.com>
2024-02-11 18:12:00 -0600

    inital commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+import os
+
+repos = [
+    'git@github.com:MisterChief53/Metapulse-wiki.git'
+]
+
+webview_url = 'git@github.com:MisterChief53/CEF-Docker-O3DE.git'
+webview_location = './WebView/tests/'
+webview_folder_name = 'cefsimple'
