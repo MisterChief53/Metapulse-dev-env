79e4d2ad85b2ac79f9bf7f665406ba46d9f057b6
MisterChief53 <soderol3@hotmail.com>
2024-02-13 07:25:49 -0600

    added script for setting each submodule to their master branch, ready for modifications

---
+++ checkout_master.py
@@ -0,0 +1,17 @@
+import os
+
+os.system("cd .\\WebView\\tests\\cefsimple")
+os.system("git fetch")
+os.system("git checkout master")
+os.system("git pull origin master")
+os.system("cd ..\\..\\..\\")
+
+os.system("cd .\\Wiki\\")
+os.system("git fetch")
+os.system("git checkout master")
+os.system("git pull origin master")
+os.system("cd ..\\")
+
+os.system("git add .")
+os.system("git commit -m \"updated submodules to latest commit, and left them at master\"")
+os.system("git push origin master")
