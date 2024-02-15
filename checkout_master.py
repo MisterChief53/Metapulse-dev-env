9ffbdf475d0f4d6d257b5ca907a9779339cdade8
MisterChief53 <soderol3@hotmail.com>
2024-02-14 18:21:38 -0600

    added accounts server and updated checkout_master

--- checkout_master.py
+++ checkout_master.py
@@ -18,6 +18,12 @@ os.system("git checkout master")
 os.system("git pull origin master")
 os.system("cd ..\\")
 
+os.system("cd .\\AccountsServer\\")
+os.system("git fetch")
+os.system("git checkout master")
+os.system("git pull origin master")
+os.system("cd ..\\")
+
 os.system("git add .")
 os.system("git commit -m \"updated submodules to latest commit, and left them at master\"")
 os.system("git push origin master")
